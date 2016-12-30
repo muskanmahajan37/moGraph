7cb3ed2e539a4e64ab9bb148e61c4a7ed4ce97dd
Fengda HUANG <h@phodal.com>
2016-12-30 15:03:09 +0800

    add draw line function

--- moGraph.js
+++ moGraph.js
@@ -38,8 +38,26 @@ moGraph.prototype.getPos = function (node) {
   return pos;
 };
 
-moGraph.prototype.drawLine = function () {
+moGraph.prototype.drawLine = function (leftPos, rightPos, key) {
+  var curveRadius = 12;
 
+  if (leftPos.y === rightPos.y) {
+    return '<line id="' + key + '" x1=' + leftPos.x + ' y1=' + leftPos.y + ' x2=' + rightPos.x + ' y2=' + rightPos.y + '/>';
+  }
+  var verticalDirection = Math.sign(rightPos.y - leftPos.y); // 1 == curve down, -1 == curve up
+  var midPointX = Math.round((leftPos.x + rightPos.x) / 2);
+  var w1 = midPointX - curveRadius - leftPos.x;
+  var w2 = rightPos.x - curveRadius - midPointX;
+  var v = rightPos.y - leftPos.y - (2 * curveRadius * verticalDirection); // Will be -ive if curve up
+  var cv = verticalDirection * curveRadius;
+
+  let pathData = 'M ' + leftPos.x + ' ' + leftPos.y
+      + ' l ' + w1 + ' 0'
+      + ' c ' + curveRadius + ' ' + 0  + ' ' + curveRadius  + ' ' + cv  + ' ' + curveRadius  + ' ' + cv
+      + ' l 0 ' + v
+      + ' c 0 ' + cv  + ' ' + curveRadius  + ' ' + cv  + ' ' + curveRadius  + ' ' + cv
+      + ' l ' + w2 + ' 0';
+  return '<path stroke-width="3" stroke="#949393" d="' + pathData + '" fill="none" />';
 };
 
 moGraph.prototype.draw = function () {
@@ -49,4 +67,14 @@ moGraph.prototype.draw = function () {
   for (var i = 0; i < data.length; i++) {
     this.createNode(data[i], options);
   }
+  var leftPos =  {
+    x: 0,
+    y: 0
+  };
+  var rightPos = {
+    x: 120,
+    y: 120
+  };
+  let line = this.drawLine(leftPos, rightPos, 'id-5-3');
+  this.canvas.appendChild(line);
 };
