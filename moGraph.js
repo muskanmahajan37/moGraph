a83a37ec246861817b10f28cd372a7ada61bab53
Fengda HUANG <h@phodal.com>
2016-12-30 12:44:42 +0800

    create basic struct

---
+++ moGraph.js
@@ -0,0 +1,23 @@
+var moGraph = function () {
+
+};
+
+moGraph.prototype.createNode = function () {
+
+};
+
+moGraph.prototype.drawNode = function () {
+
+};
+
+moGraph.prototype.createEdge = function () {
+
+};
+
+moGraph.prototype.updateLayout = function () {
+
+};
+
+moGraph.prototype.drawLine = function () {
+
+};
